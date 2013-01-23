07b4cc8d7fad20481578df384556b7d2fe9e6d5b
wanabe <s.wanabe@gmail.com>
2013-01-23 23:07:28 +0900

    add mov(pc, lr) for debug

--- xtaak/xtaak.h
+++ xtaak/xtaak.h
@@ -175,6 +175,15 @@ public:
 class CodeGenerator : public CodeArray {
 public:
 	const Reg fp, ip, sp, lr, pc, spW;
+	void mov(const Operand& reg1, const Operand& reg2)
+	{
+		if (reg1.isREG() && reg1.getIdx() == Operand::PC &&
+		    reg2.isREG() && reg2.getIdx() == Operand::LR) {
+			top_[size_++] = 0xe1a0f00e;
+		} else {
+			throw ERR_NOT_IMPL;
+		}
+	}
 public:
 	CodeGenerator(size_t maxSize = DEFAULT_MAX_CODE_SIZE, void *userPtr = 0, Allocator *allocator = 0)
 		: CodeArray(maxSize, userPtr, allocator)
